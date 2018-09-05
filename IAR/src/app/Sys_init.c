6e561438be8646aab26b5c8438b249e510f4cd76
zhang12300 <2943452451@qq.com>
2018-09-05 23:42:13 +0800

    delete

--- IAR/src/app/Sys_init.c
+++ IAR/src/app/Sys_init.c
@@ -4,14 +4,14 @@ void ClockGate_init()
 {
   SIM_SCGC5 |=
     (SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTC_MASK
-     | SIM_SCGC5_PORTD_MASK | SIM_SCGC5_PORTE_MASK);    //开全部端口时钟 system clock gating control
+     | SIM_SCGC5_PORTD_MASK | SIM_SCGC5_PORTE_MASK);    //开全部端口时钟
 }
 
 void System_init(void)
 {
   DisableInterrupts;
   ClockGate_init();
-  SYSMPU->CESR &= ~SYSMPU_CESR_VLD_MASK;//SYSMPU  system memory protector unit (MPU )  CESR: control error status register  ;  VLD =0 MPU disable 
+  SYSMPU->CESR &= ~SYSMPU_CESR_VLD_MASK;
   
   EnableInterrupts;  
 }
